17c87708d0e49e15ed1923d5102026041e7bc34a
seventeenbrother <1779929713@qq.com>
2019-06-21 21:52:03 +0800

    实现发送验证登陆实现跳转

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,27 +1,7 @@
 <template>
-  <div id="home">
-    <el-form class="demo-ruleForm">
-        <img src="../assets/logo_index.png" alt="">
-      <el-row :gutter="20">
-        <el-col :span="24">
-          <el-input v-model="mobile" placeholder="请输入手机号"></el-input>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="14">
-          <el-input v-model="code" placeholder="请输入验证码"></el-input>
-        </el-col>
-        <el-col :span="8">
-          <el-button>获取验证码</el-button>
-        </el-col>
-      </el-row>
-      <el-row :gutter="20">
-        <el-col :span="24">
-          <el-button class="login">登录</el-button>
-        </el-col>
-      </el-row>
-    </el-form>
-  </div>
+ <div id="home">
+     {{ home }}
+ </div>
 </template>
 
 <script>
@@ -29,39 +9,12 @@ export default {
   name: 'AppHome',
   data () {
     return {
-      mobile: '',
-      code: ''
+      home: 'AppHome'
     }
   }
 }
 </script>
 
 <style lang='less' scoped>
-  #home {
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-image: url(../assets/login_bg.jpg);
-    background-size: 1366px;
-  }
-  .el-form{
-    width: 300px;
-    padding: 30px;
-    text-align: center;
-    background-color: #fff;
-}
-  .el-input {
-    width: 100%;
-    margin-bottom: 10px;
-  }
 
-  img {
-      width: 70%;
-      margin-bottom: 10px;
-  }
-  .login {
-      width: 100%;
-      background-color: skyblue;
-  }
 </style>
